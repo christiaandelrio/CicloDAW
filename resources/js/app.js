459fabf0e8c6d1ebacc4eb6ee181db2ced3be7d9
Christian Del Río <delrio.lemos49@gmail.com>
2024-12-19 12:50:21 +0000

    Compilar todos los archivos JavaScript en app.js correctamente

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+// resources/js/app.js
+
+require('./datatable.js');
+require('./modal.js');
+require('./primerospasos.js');
+require('./options-menu.js');
+require('./scroll.js');
+require('./graficas.js');
+require('./popup.js');
